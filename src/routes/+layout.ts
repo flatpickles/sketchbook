712c0fc0e2cf276ce801f0b1ba647c97a80da695
Matt Nichols <mattnichols206@gmail.com>
2023-06-12 22:28:21 -0700

    Public/private project properties

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,7 +5,10 @@ export const load: PageLoad = (() => {
 	for (const path in projects) {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		projects[path]().then((module: any) => {
-			new module.default();
+			const instance = new module.default();
+			console.log(Object.getOwnPropertyNames(instance));
+			instance.publicValue = 'new value';
+			instance.logPublicValue();
 		});
 	}
 
