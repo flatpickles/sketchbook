330eb13d4806c20d034facb74f274ff29274651d
Matt Nichols <mattnichols206@gmail.com>
2023-06-12 22:42:53 -0700

    Multi-file projects

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,14 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = (() => {
-	const projects = import.meta.glob('../projects/*.ts');
+	const projects = import.meta.glob('../projects/**/*.ts');
+	console.log(projects);
 	for (const path in projects) {
+		// Project files are named the same as their containing folder
+		const pathComponents = path.split('/');
+		const name = pathComponents.pop()?.split('.')[0];
+		if (name && pathComponents.indexOf(name) < 0) continue;
+
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		projects[path]().then((module: any) => {
 			const instance = new module.default();
