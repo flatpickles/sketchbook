4d85a4e0a6f927805cf33956cb2384674bd98b04
Matt Nichols <mattnichols206@gmail.com>
2023-08-02 14:36:04 -0700

    Header button layout

--- src/lib/components/PanelHeader.svelte
+++ src/lib/components/PanelHeader.svelte
@@ -5,11 +5,14 @@
 </script>
 
 <div class="header-wrapper">
-    <div class="header-lede">
-        <h1 data-testid="header-title">{title}</h1>
-        {#if subtitle}
-            <h2 data-testid="header-subtitle">{@html subtitle}</h2>
-        {/if}
+    <div class="header-top">
+        <div class="header-lede">
+            <h1 data-testid="header-title">{title}</h1>
+            {#if subtitle}
+                <h2 data-testid="header-subtitle">{@html subtitle}</h2>
+            {/if}
+        </div>
+        <button class="header-button"><i class="fa fa-close" /></button>
     </div>
 
     {#if description}
@@ -19,21 +22,36 @@
 
 <style lang="scss">
     .header-wrapper {
-        padding: $panel-content-inset $panel-content-inset 0;
         display: flex;
         flex-direction: column;
         gap: $panel-header-section-spacing;
     }
 
+    .header-top {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
+        gap: $panel-header-section-spacing;
+    }
+
+    .header-button {
+        @include header-button;
+        flex-shrink: 0;
+    }
+
     h1 {
         @include title-text;
+        padding: $panel-content-inset $panel-content-inset 0;
     }
 
     h2 {
         @include subtitle-text;
+        padding: 0 $panel-content-inset;
     }
 
     p {
         @include description-text;
+        padding: 0 $panel-content-inset;
     }
 </style>
