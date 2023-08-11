74b25085bd5b21e2464ace5f3273b18e12ec0a3c
Matt Nichols <mattnichols206@gmail.com>
2023-08-11 10:18:58 -0700

    Fix panel header title wrapping

---
+++ src/art/AllParams/AllParams.ts
@@ -0,0 +1,15 @@
+import Project from '$lib/base/Project';
+
+export default class AllParams extends Project {
+    // todo: more params!
+    number = 0.1;
+    numericArray = [0.5, 0.5];
+    boolean = true;
+    string = 'hello world';
+    function = () => {
+        alert('hello!');
+    };
+    fileInput = async (result: string) => {
+        console.log(result);
+    };
+}
