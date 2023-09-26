3d402ed1a77b271312c33deb5e4c555f1fbe2c02
Matt Nichols <mattnichols206@gmail.com>
2023-09-26 11:01:58 -0700

    MVP error page; lightweight project file text validation for availability

---
+++ src/routes/+error.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<div class="error-wrapper">
+    <div class="error-display">
+        <div class="error-header">Error</div>
+        <div class="error-message">{$page.error?.message}</div>
+    </div>
+</div>
+
+<style lang="scss">
+    .error-wrapper {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .error-display {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: #f5f5f5;
+        border: 2px solid #000;
+    }
+
+    .error-header {
+        padding: 0.5rem;
+        font-weight: bold;
+        font-size: $title-text-size;
+    }
+
+    .error-message {
+        padding: 1rem;
+        border-top: 2px solid #000;
+        font-size: $large-text-size;
+    }
+</style>
