e1fa09b0a2ba53079aa753503f1c84cb0d2677b6
Matt Nichols <mattnichols206@gmail.com>
2023-09-27 13:36:41 -0700

    Define types for Project lifecycle detail params

--- src/lib/base/Project/Project.ts
+++ src/lib/base/Project/Project.ts
@@ -38,26 +38,19 @@ export default class Project {
 
     /**
      * update is called continuously in a requestAnimationFrame loop. Override this with your custom
-     * drawing code. The detail object contains the following:
-     * - frame: the current frame number (0 at project load, incremented by 1 for each update call)
-     * - time: milliseconds passed since project load (i.e. since init was called)
+     * drawing code.
      */
-    public update(detail: { frame: number; time: number }) {}
+    public update(detail: UpdateDetail) {}
 
     /**
-     * paramChanged is called when a parameter is changed in the UI. The detail object contains the
-     * following:
-     * - paramKey: the key of the parameter that was changed
+     * paramChanged is called when a parameter is changed in the UI.
      */
-    public paramChanged(detail: { paramKey: string }) {}
+    public paramChanged(detail: ParamChangedDetail) {}
 
     /**
-     * resized is called when the container div and/or active canvas is resized. The detail object
-     * contains the following:
-     * - containerSize: the new size of the container div element, in pixels
-     * - canvasSize: the new size of the canvas element, in pixels (if using a canvas)
+     * resized is called when the container div and/or active canvas is resized.
      */
-    public resized(detail: { containerSize: [number, number]; canvasSize?: [number, number] }) {}
+    public resized(detail: ResizedDetail) {}
 
     /**
      * destroy is called when the project is unloaded, i.e. when another project is selected.
@@ -80,3 +73,28 @@ export enum CanvasType {
     WebGL = 'webgl',
     None = 'none'
 }
+
+/**
+ * Detail object passed to project's Update method. Contains the following:
+ * - frame: the current frame number (0 at project load, incremented by 1 for each update call).
+ * - time: milliseconds passed since project load (i.e. since init was called).
+ */
+export type UpdateDetail = { frame: number; time: number };
+
+/**
+ * Detail object passed to project's paramChanged method. Contains the following:
+ * - paramKey: the key of the parameter that was changed.
+ */
+export type ParamChangedDetail = { paramKey: string };
+
+/**
+ * Detail object passed to project's resized method. Contains the following:
+ * - containerSize: the new size of the container div element, in pixels.
+ * - canvasSize: the new size of the canvas element, in pixels (if using a canvas).
+ * Note that canvasSize is not likely to correspond to [this.canvas.width, this.canvas.height], as
+ * the canvas drawing size is scaled by the current pixel ratio.
+ */
+export type ResizedDetail = {
+    containerSize: [number, number];
+    canvasSize?: [number, number];
+};
