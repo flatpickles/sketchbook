88439a54390ddc5bad5a6b5c01c019078de80720
Matt Nichols <mattnichols206@gmail.com>
2023-10-01 15:09:46 -0700

    P5 Demo project

---
+++ src/art/P5Demo/P5Demo.ts
@@ -0,0 +1,43 @@
+import P5Project from '$lib/base/Project/P5Project';
+import type { ParamChangedDetail, ResizedDetail } from '$lib/base/Project/Project';
+import type P5 from 'p5';
+
+type ColorArray = [number, number, number];
+
+export default class P5Demo extends P5Project {
+    circleBaseSize = 50;
+    circleSizeVariation = 50;
+    bgColor: ColorArray = [123, 43, 21];
+    resetCanvas = () => {
+        this.p5?.background(...this.bgColor);
+    };
+
+    // p5.js methods:
+
+    setup() {
+        this.resetCanvas();
+    }
+
+    mouseMoved(p5: P5) {
+        const circleColor: ColorArray = [
+            Math.random() * 255,
+            Math.random() * 255,
+            Math.random() * 255
+        ];
+        const circleSize = this.circleBaseSize + Math.random() * this.circleSizeVariation;
+        p5.fill(...circleColor);
+        p5.circle(p5.mouseX, p5.mouseY, circleSize);
+    }
+
+    // Sketchbook methods:
+
+    resized(detail: ResizedDetail) {
+        super.resized(detail);
+        this.resetCanvas();
+    }
+
+    paramChanged(detail: ParamChangedDetail) {
+        super.paramChanged(detail);
+        if (detail.key === 'bgColor') this.resetCanvas();
+    }
+}
