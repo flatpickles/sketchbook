aa93b7fa24b06a0628e805b6fe2f84568d8a2e87
Matt Nichols <mattnichols206@gmail.com>
2023-10-12 12:11:15 -0700

    Copy arrays when applying presets

---
+++ src/art/Project3/Project3.ts
@@ -0,0 +1,8 @@
+import Project from '$lib/base/Project/Project';
+
+export default class Project3 extends Project {
+    number = 0; // 1 to 10
+    string = 'yaas';
+    boolean = false;
+    array = [1, 2, 3]; // 1 to 3
+}
